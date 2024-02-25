e9ce6ff2e37942b118e5a579272d6509dd503f8f
Arseny Boykov <36469655+Bobronium@users.noreply.github.com>
2024-02-25 17:40:46 +0300

    Add tests for encode/decode error

--- tests/test_base32.py
+++ tests/test_base32.py
@@ -1,3 +1,4 @@
+import pytest
 from typeid.base32 import decode, encode
 
 
@@ -13,3 +14,16 @@ def test_encode_decode_logic() -> None:
     decoded_data = decode(encoded_data)
 
     assert decoded_data == original_data
+
+@pytest.mark.parametrize("value", (b"1" * 15, b"1" * 17, b""))
+def test_encode_errors(value) -> None:
+    
+    with pytest.raises(RuntimeError):
+        encoded_data = encode(value)
+
+@pytest.mark.parametrize("value", ("00041061050r3gg28a1c60t3gf1", "123", "1"))
+def test_encode_errors(value) -> None:
+    
+    with pytest.raises(RuntimeError):
+        encoded_data = encode(value)
+        
